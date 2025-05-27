1ccfa563f04a3cfe993637ea7a9e01dec2b84641
Natallia <bukato.natallia@gmail.com>
2025-05-27 22:19:22 +0300

    1 task

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -1,5 +1,13 @@
 // С помощью цикла while найдите сумму чисел от 1 до 20
 // Результат присвойте переменной sum
 
+let sum=0;
+let num=1;
+
+while (num<=20) {
+    sum+=num;
+    num++;
+} 
+
 // your code
-let sum
+
