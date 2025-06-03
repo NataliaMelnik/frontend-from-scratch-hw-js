9053e491957fb337a8bc992ea02d6be290e279af
Natallia <bukato.natallia@gmail.com>
2025-06-03 21:50:55 +0300

    1 task

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,26 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-let isAccess
+let isAccess;
+
+if (
+        (isAdmin===true || isVerifiedUser===true) && 
+        (hasSpecialPermission===true || hasTemporaryPass===true)
+    ) {
+
+        isAccess=true;
+} else {
+
+        isAccess=false;
+}
+
+console.log(isAccess);
+
+
 
 // your code
