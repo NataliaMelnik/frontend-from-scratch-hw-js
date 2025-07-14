a4cab572944ae6cbebc9a2cf28250465e33c9727
Natallia <bukato.natallia@gmail.com>
2025-07-14 21:58:18 +0300

    1 task

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,22 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+  
+  let arr = str.split(' ');
+  let result = [];
+  
+  for (let i=0; i<arr.length; i++) {
+      let word = arr[i].split('');
+      word[0] = word[0].toUpperCase();
+      word = word.join('');
+      result.push(word);
+            
+  }
+  
+  let res = result.join(' ');
+   
+  return res;
+}
+
+capitalizeWords('hello world from javascript');
